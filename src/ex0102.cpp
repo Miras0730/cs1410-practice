4eb335a6ae08a8b6f0dbc69e4a042c5f7c58399f
KazimQuraishi <kazimquraishi@mail.weber.edu>
2025-05-13 05:52:38 +0000

    finished ex0102

---
+++ src/ex0102.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    double c, f;
+    cout << "tempreture in Celsius: " << endl;
+    cin >> c;
+
+    f = 9.0 * c / 5.0 +32;
+    cout << "Temprature in Fahrenheit: " << f << endl;
+    return 0;
+}
